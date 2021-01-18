4ad37ba95a54af9d1cbc55c07a548e1a37811d79
faith pueneh <puenehfaith@gmail.com>
2021-01-18 08:17:35 +0100

    First update

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,94 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>url-shortner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className={styles.header}>
+        <div className={styles.nav}>
+          <div>
+            <ul className={styles.nav_list}>
+              <li>
+                <img src="/logo.svg" alt="Logo" className={styles.logo_top} />{" "}
+              </li>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Resources</li>
+            </ul>
+          </div>
+          <div className={styles.btn}>
+            <button className={styles.btn_gray}>Login</button>
+            <button className={styles.btn_green}> Sign Up</button>
+          </div>
+        </div>
+      </header>
+
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className={styles.hero}>
+          <div>
+            <h1 className={styles.title}>More than just shorter links</h1>
+            <p className={styles.description}>
+              Build your brand’s recognition and get detailed insights on how
+              your links are performing.
+            </p>
+            <button className={styles.btn_white}>Get Started</button>
+          </div>
+          <div>
+            <img
+              src="/illustration-working.svg"
+              alt="working"
+              className={styles.logo_hero}
+            />
+          </div>
+        </div>
+        <div className={styles.short}>
+          <input
+            // defaultValue="Shorten a link here"
+            placeholder="Shorten a link here"
+            id="link_shortner"
+            className={styles.input}
+            type="search"
+          ></input>
+          <button className={styles.btn_shorten}>Shorten It!</button>
+        </div>
+        <div className={styles.features}>
+          <h2>Advanced Statistics</h2>
+          <p>
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+          <div className={styles.grid}>
+          <div className={styles.card}>
+            <h3>Brand Recognition</h3>
+            <p>Boost your brand recognition with each click. Generic links don’t 
+  mean a thing. Branded links help instil confidence in your content.</p>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <div className={styles.card}>
+            <h3>Detailed Records</h3>
+            <p>Gain insights into who is clicking your links. Knowing when and where 
+  people engage with your content helps inform better decisions.</p>
+          </div>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3> Fully Customizable</h3>
+            <p>Improve brand awareness and content discoverability through customizable 
+  links, supercharging audience engagement.</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          
         </div>
+        </div>
+
+        
       </main>
 
       <footer className={styles.footer}>
@@ -56,10 +97,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{" "}
+          <img src="/logo.svg" alt="shortly Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
