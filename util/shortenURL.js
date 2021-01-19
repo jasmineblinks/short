409c0a2991c976f213107fb377c0b426d776c4b6
faith pueneh <puenehfaith@gmail.com>
2021-01-19 14:45:23 +0100

    updated

--- util/shortenURL.js
+++ util/shortenURL.js
@@ -2,5 +2,5 @@
 export default async function shortenURL(value){
    const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${value}`)
   const data = await res.json();
-  console.log(data.result.short_link2)     
+  console.log(data)     
 }
